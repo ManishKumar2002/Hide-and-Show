9697b25436baabc1a1c2b361f56646ac3b9e4d8e
Manish Kumar <manish10akbr@gmail.com>
2022-12-08 02:51:18 +0530

    project files

---
+++ Assets/hideShow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hideShow : MonoBehaviour
+{
+    public void show(GameObject img)
+    {
+        img.SetActive(true);
+    }
+    public void hide(GameObject img)
+    {
+        img.SetActive(false);
+    }
+
+}
